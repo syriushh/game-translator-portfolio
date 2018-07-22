167225e2c95b137ce6dca812cca6b43ead33b1f3
Bartek <aqfgit@gmail.com>
2018-07-22 14:38:26 +0200

    Add functionality for opening and closing the image modal

--- dist/script-compiled.js
+++ dist/script-compiled.js
@@ -1,11 +1,64 @@
 'use strict';
 
-var test = 'dzila';
+var itemImageWrappers = document.querySelectorAll('.js-modal-trigger');
 
-alert(test);
+var modal = document.getElementById('modal');
+var modalTitle = document.getElementById('modalTitle');
+var modalImage = document.getElementById('modalImage');
+var modalContainer = document.getElementById('modalContainer');
 
-var add = function add(a, b) {
-  return a + b;
-};
+function loadContentToMoadl(image, title) {
+  modalImage.src = image;
+  modalTitle.innerHTML = title;
+}
+
+function getImageForModal(element) {
+  if (element.tagName === 'IMG') {
+    return element;
+  } else {
+    // if user clicked on image wrapper
+    return element.childNodes[1];
+  }
+}
+
+function openModal(event) {
+  var image = getImageForModal(event.target);
+  loadContentToMoadl(image.src, image.alt);
+  modal.classList.add('modal--open');
+}
+
+function closeModal(event) {
+  var clickedBeyondTheImage = event.target.classList.contains('js-no-closing') === false;
+  if (clickedBeyondTheImage) {
+    modal.classList.remove('modal--open');
+  }
+}
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+  for (var _iterator = itemImageWrappers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+    var item = _step.value;
+
+    item.addEventListener('click', openModal, true);
+  }
+} catch (err) {
+  _didIteratorError = true;
+  _iteratorError = err;
+} finally {
+  try {
+    if (!_iteratorNormalCompletion && _iterator.return) {
+      _iterator.return();
+    }
+  } finally {
+    if (_didIteratorError) {
+      throw _iteratorError;
+    }
+  }
+}
+
+modalContainer.addEventListener('click', closeModal, true);
 
 //# sourceMappingURL=script-compiled.js.map
--- src/app.js
+++ src/app.js
@@ -1,5 +1,38 @@
-const test = 'dzila'
+const itemImageWrappers = document.querySelectorAll('.js-modal-trigger');
 
-alert(test)
+const modal = document.getElementById('modal');
+const modalTitle = document.getElementById('modalTitle');
+const modalImage = document.getElementById('modalImage');
+const modalContainer = document.getElementById('modalContainer')
 
-let add = (a, b) => a + b  
+function loadContentToMoadl(image, title) {
+  modalImage.src = image
+  modalTitle.innerHTML = title;
+}
+
+function getImageForModal(element) {
+  if (element.tagName === 'IMG') {
+    return element;
+  } else {  // if user clicked on image wrapper
+    return element.childNodes[1];
+  }
+}
+
+function openModal(event) {
+  const image = getImageForModal(event.target);
+  loadContentToMoadl(image.src, image.alt)
+  modal.classList.add('modal--open');
+}
+
+function closeModal(event) {
+  const clickedBeyondTheImage = event.target.classList.contains('js-no-closing') === false
+  if (clickedBeyondTheImage) {
+    modal.classList.remove('modal--open');
+  }
+}
+
+for (let item of itemImageWrappers) {
+  item.addEventListener('click', openModal, true);
+}
+
+modalContainer.addEventListener('click', closeModal, true)
